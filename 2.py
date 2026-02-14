316618c3e97bf509e4e9d4870d0b836359824524
Brooks <coderoom52@gmail.com>
2026-02-14 13:11:25 +0530

    solved 2nd exerise

---
+++ 2.py
@@ -0,0 +1,9 @@
+# 2. Python Version Checker
+
+# Write a Python program to find out what version of Python you are using.
+
+import sys
+a = sys.version
+b = sys.version_info
+print(f"Python version is {a}")
+print(f"Python version INFO is {b}")
