3d5260c6e63f72e9c78031f50018307604141c6a
Brooks <coderoom52@gmail.com>
2026-02-14 13:48:53 +0530

    Solved 5th exercise

---
+++ 5.py
@@ -0,0 +1,9 @@
+# 5. Reverse Full Name
+
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them
+
+print("--------------  Reverse Full Name -----------")
+
+name = input("Enter your First Name : ")
+surname = input("Enter your surname : ")
+print("Hello,"+ surname + " " + name)
