819860998341c1b59ec67573bfcf0b8ea7843acc
Brooks <coderoom52@gmail.com>
2026-02-14 19:07:10 +0530

    Solved 12th exercise

---
+++ 12.py
@@ -0,0 +1,13 @@
+# 13. Multi-line Here Document
+
+# Write a Python program to print the following 'here document'.
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+a = '''a string that you "don't" have to escape
+ This
+ is a ....... multi-line
+ heredoc string --------> example'''
+print(a)
