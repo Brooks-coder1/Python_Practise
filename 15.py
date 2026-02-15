7838869c9b6f33d81b17091e87e71f90c5589009
Brooks <coderoom52@gmail.com>
2026-02-15 20:28:39 +0530

    solved 15th exercise

---
+++ 15.py
@@ -0,0 +1,7 @@
+# 15. Sphere Volume Calculator
+
+# Write a Python program to get the volume of a sphere with radius six.
+
+user = int(input("Enter the radius : "))
+a = 4/3*22/7*(user**3)
+print(f"The volume of sphere is {a}")
