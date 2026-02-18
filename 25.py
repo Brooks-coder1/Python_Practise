047e578c7501819a3d06368b9e9d1c27500b467b
Brooks <coderoom52@gmail.com>
2026-02-18 19:12:53 +0530

    solved 25th exercise

---
+++ 25.py
@@ -0,0 +1,21 @@
+def container(a):
+    if a in [1, 5, 8, 3]:
+        print(True)
+    else:
+        print(False)
+
+
+while True:
+    user_input = input("Enter a number (or type 'exit' to quit): ")
+
+    # break condition
+    if user_input.lower() == "exit":
+        print("Program terminated.")
+        break
+
+    try:
+        a = int(user_input)
+        container(a)
+
+    except ValueError:
+        print("Oops! Please enter a valid number or 'exit'.")
